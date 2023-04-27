8ccb82b883ee79cfc8f12438dfee08cce3a8afae
AnhND <anhndhe161107@fpt.edu.vn>
2023-04-28 02:11:19 +0700

    Update 28/4/2023

--- src/pages/navbar/navbar.js
+++ src/pages/navbar/navbar.js
@@ -1,5 +1,6 @@
 import logo from "../../images/Layer 1.png";
 import cart from "../../images/Icon Cart.png";
+import dropdown from "../../images/dropdown.png";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 export default function Navbar() {
@@ -8,15 +9,28 @@ export default function Navbar() {
       <div className="navbar">
         <img src={logo} alt="logo"></img>
         <div className="navbar-category">
-          <Link to='/#'>Level Test</Link>
-          <Link to='/#'>Courses</Link>
-          <Link to='/#'>Learning Resources</Link>
-          <Link to='/aboutUs'>About us</Link>
+          <div className="learningResources">
+            <Link to="/#">Level Test</Link>
+          </div>
+          <div className="learningResources">
+            <Link to="/#">Courses</Link>
+          </div>
+          <div className="learningResources">
+            <Link to="/#">Learning Resources</Link>
+            <img src={dropdown} alt="dropdown"></img>
+          </div>
+          <div className="learningResources">
+            <Link to="/aboutUs">About us</Link>
+          </div>
         </div>
         <div className="navbar-right">
           <img src={cart} alt="cart"></img>
-          <Link to='/'><button id="btn-left">Sign in</button></Link>
-          <Link to='/register'><button>Sign up</button></Link>
+          <Link to="/">
+            <button id="btn-left">Sign in</button>
+          </Link>
+          <Link to="/register">
+            <button>Sign up</button>
+          </Link>
         </div>
       </div>
     </>
